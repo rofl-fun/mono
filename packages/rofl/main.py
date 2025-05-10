cd6290fb2a4304a39496ce6b6d28f373a0945c26
jacksmithinsulander <666.jack.smith@protonmail.com>
2025-05-10 15:37:24 +0200

    fixes

--- packages/rofl/main.py
+++ packages/rofl/main.py
@@ -1,27 +1,27 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-app = FastAPI()
+# app = FastAPI()
 
-@app.get("/")
-def idle():
-    return {"status": "i'm alive"}
+# @app.get("/")
+# def idle():
+    # return {"status": "i'm alive"}
 
-@app.post("/v1/join")
-def join_group():
-    pass
+# @app.post("/v1/join")
+# def join_group():
+    # pass
 
-@app.post("/v1/leave")
-def leave_group():
-    pass
+# @app.post("/v1/leave")
+# def leave_group():
+    # pass
 
-@app.post("/v1/message")
-def message():
-    pass
+# @app.post("/v1/message")
+# def message():
+    # pass
 
-@app.get("/v1/chatfeedOf/{user}")
-def get_chat_feed_of(user: str):
-    pass
+# @app.get("/v1/chatfeedOf/{user}")
+# def get_chat_feed_of(user: str):
+    # pass
 
-@app.get("/v1/historyOf/{chat}")
-def get_history_of(chat: str):
-    pass
+# @app.get("/v1/historyOf/{chat}")
+# def get_history_of(chat: str):
+    # pass
